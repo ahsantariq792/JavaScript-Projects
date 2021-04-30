46ebe3e46ac14407a7dd49edc204819c1898b191
Ahsan Tariq <61455261+ahsantariq792@users.noreply.github.com>
2021-05-01 00:12:36 +0500

    number loop

---
+++ 16880-A2_numberLoop/app.js
@@ -0,0 +1,96 @@
+for(var i=1;i<=5;i++){
+    for(var j=1;j<=i;j++){
+        document.write(j)
+    }
+    document.write("<br>")
+
+}
+
+
+
+document.write("<br>")
+document.write("<br>")
+document.write("<br>")
+document.write("<br>")
+
+
+
+for(var i=1;i<=5;i++){
+    for(var j=1;j<=5;j++){
+        document.write(i)
+    }
+    document.write("<br>")
+
+}
+
+
+
+
+
+
+document.write("<br>")
+document.write("<br>")
+document.write("<br>")
+document.write("<br>")
+
+
+
+
+for(var i=1;i<=5;i++){
+    for(var j=1;j<=i;j++){
+        document.write(i)
+    }
+    document.write("<br>")
+
+}
+
+
+
+document.write("<br>")
+document.write("<br>")
+document.write("<br>")
+document.write("<br>")
+
+
+
+for(var i=1;i<=5;i++){
+    for(var j=1;j<=5;j++){
+        document.write(i)
+    }
+    document.write("<br>")
+
+}
+
+document.write("<br>")
+document.write("<br>")
+document.write("<br>")
+document.write("<br>")
+
+
+
+
+for(var i=5;i>=1;i--){
+    for(var j=5;j>=i;j--){
+        document.write(j)
+    }
+    document.write("<br>")
+}
+
+
+
+
+document.write("<br>")
+document.write("<br>")
+document.write("<br>")
+document.write("<br>")
+
+
+
+
+
+for (var i=1;i<100;i=i+10){
+    for(var j=i;j<i+10;j++){
+        document.write(" ",j);
+    }
+    document.write("<br>")
+}
