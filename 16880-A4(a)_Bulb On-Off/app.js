28255be4218a058de37b65cb1f6b69b018649af7
Ahsan Tariq <61455261+ahsantariq792@users.noreply.github.com>
2021-05-04 20:38:37 +0500

    Bulb On and Off by two buttons
    Bulb On and Off by using two different buttons

---
+++ 16880-A4(a)_Bulb On-Off/app.js
@@ -0,0 +1,9 @@
+function bulbOn(){
+    var image = document.getElementById("bulbOff")
+    image.src = "images/bulb-on.jpg"
+}
+
+function bulbOff(){
+    var image = document.getElementById("bulbOff")
+    image.src = "images/bulb-off.jpg"
+}
